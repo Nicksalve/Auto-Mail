42505ef57de220838994f911b8b63860918d8f76
Nick <70932969+Nicksalve@users.noreply.github.com>
2021-06-21 10:26:19 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,9 +1,2 @@
-#type in terminal 'pip install secure-smtplib'
-import smtplib
-server = smtplib.SMTP('smtp.gmail.com',587)
-server.starttls()
-server.login('ayush.tech100@gmail.com','ayush.techsavy')
-server.sendmail('ayush.tech100@gmail.com',
-               'ayush.tech100@gmail.com',
                input(“type your message”)
                )
