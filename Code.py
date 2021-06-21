05902b8227e6d9e4a2054f48b5f8df48474ac18f
Nick <70932969+Nicksalve@users.noreply.github.com>
2021-06-21 10:21:58 +0530

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,8 @@
+import smtplib
+server = smtplib.SMTP('smtp.gmail.com',587)
+server.starttls()
+server.login('ayush.tech100@gmail.com','ayush.techsavy')
+server.sendmail('ayush.tech100@gmail.com',
+               'ayush.tech100@gmail.com',
+               input(“type your message”)
+               )
