5acbec1bc7b2a22647ffb7333511e501a7006cb6
Nick <70932969+Nicksalve@users.noreply.github.com>
2021-06-21 10:34:32 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,2 +1,10 @@
-               input(“type your message”)
+#type this command in your teminal 'pip install secure-smtplib'
+#go to your google account, go to security and turn on 'Less secure app access'
+import smtplib
+server = smtplib.SMTP('smtp.gmail.com',587)
+server.starttls()
+server.login('your email ID','Password')
+server.sendmail('recivers email ID',
+                'recivers email ID',
+                input(“type your message”)
                )
